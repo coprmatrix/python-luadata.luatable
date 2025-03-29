e0db7d1be65342bb10864e5b662574084fd3b7a0
u <u>
2025-03-29 21:07:10 +0300

    update

--- reg.pl
+++ reg.pl
@@ -1,6 +1,6 @@
 my $name = <<'EOF';
 %files -n %{python_name}
-%{python3_sitelib}/%{pypi_name}-%{version}.dist-info
+%{python3_sitelib}/%{pypi_name}*%{version}.dist-info
 %{python3_sitelib}/luadata/luatable/__init__.py
 %{python3_sitelib}/luadata/luatable/__pycache__/*
 EOF
