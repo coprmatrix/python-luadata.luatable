03dade7b09029a6877fb266b1af3793d921e0e22
u <u>
2025-03-29 21:26:00 +0300

    update

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ cname="$(echo $name | sed 's/\./_/g;s/-/_/g')"
 (
 cd *"$cname"
 python3 -m build -s -n -o .
-py2pack generate "$name" --localfile ./"$name".egg-info/PKG-INFO --source-url 'https://files.pythonhosted.org/packages/source/l/luadata_luatable/luadata_luatable-0.0.3.tar.gz'
+py2pack generate "$name" --localfile ./"$name".egg-info/PKG-INFO --source-url 'https://files.pythonhosted.org/packages/source/l/luadata_luatable/luadata_luatable-%{version}.tar.gz'
 sed -i "s~%{pypi_name}-%{version}~luadata_luatable-%{version}~;" *.spec
 )
 mv *"$cname"/*.{spec,tar.gz} "$outdir"/
